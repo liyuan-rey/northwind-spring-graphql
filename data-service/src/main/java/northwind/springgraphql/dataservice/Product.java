0568bd714cbe98ee5368c2a3a1d615a688caae6b
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2021-11-05 16:14:35 +0800

    update: 补充 lombok 构造函数注解，修改测试类名以更符合规范，补充 schema

--- data-service/src/main/java/northwind/springgraphql/dataservice/Product.java
+++ data-service/src/main/java/northwind/springgraphql/dataservice/Product.java
@@ -9,10 +9,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
--- data-service/src/test/java/northwind/springgraphql/dataservice/ProductRepositoryJpaTests.java
+++ data-service/src/test/java/northwind/springgraphql/dataservice/ProductRepositoryJpaTests.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-public class ProductRepositoryTest {
+public class ProductRepositoryJpaTests {
     @Autowired
     private ProductRepository pr;
 
